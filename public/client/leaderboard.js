ee97dd337bdd0a1c1aa5122bea5dcf65258cb1a9
BlismaC <97406250+BlismaC@users.noreply.github.com>
2025-07-14 22:06:17 +0200

    Update leaderboard.js

--- public/client/leaderboard.js
+++ public/client/leaderboard.js
@@ -50,7 +50,7 @@ export function updateLeaderboard(players, myId) {
 
         const nameSpan = document.createElement('span');
         nameSpan.classList.add('name');
-        nameSpan.textContent = player.name || "Unnamed";
+        nameSpan.textContent = (player.name || "Unnamed") + document.getElementById('skullIcon');;
         
         if (player.id === myId) {
             nameSpan.style.fontWeight = 'bold';
