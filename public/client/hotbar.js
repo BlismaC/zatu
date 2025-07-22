f7673e5d1745341ebcaf50b4659b89395b3ac77a
BlismaC <97406250+BlismaC@users.noreply.github.com>
2025-07-22 12:28:55 +0200

    Update hotbar.js

--- public/client/hotbar.js
+++ public/client/hotbar.js
@@ -229,4 +229,4 @@ if (!CanvasRenderingContext2D.prototype.roundRect) {
 }
 
 // Export hotbar and the new setter function
-export { hotbar, getActiveItem, setActiveSlotIndex, activeSlotIndex }; // Removed activeSlotIndex direct export
+export { hotbar, getActiveItem, setActiveSlotIndex }; // Removed activeSlotIndex direct export
