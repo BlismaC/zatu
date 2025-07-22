ea3972de71e04a8a34394d5260cb1530dfa18cc4
BlismaC <97406250+BlismaC@users.noreply.github.com>
2025-07-22 10:38:21 +0200

    Update main.js

--- public/client/main.js
+++ public/client/main.js
@@ -288,6 +288,8 @@ function startGame() {
     // Global keydown/keyup listeners for chat and general game input
    document.addEventListener("keydown", (e) => {
     const currentTime = Date.now();
+    handleHotbarInput();
+    
     //  const isMainMenuVisible = mainMenu.style.display === 'block' ? true : false;
     const isChatInputFocused = localChatInput && document.activeElement === localChatInput;
     const me = players[myId];
